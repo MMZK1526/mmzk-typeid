0a3915d4e86a66af07bba636603a5ceb8848c7fc
mmzk1526 <yc4120@ic.ac.uk>
2024-06-25 23:26:56 +0100

    Implement KindID errors as custom compilation errors (some instances are not declared yet) Hide irrelevant type-level helpers from the documentation

--- src/Data/KindID/Class.hs
+++ src/Data/KindID/Class.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE UndecidableInstances #-}
 
 -- |
@@ -14,25 +15,16 @@ module Data.KindID.Class
   -- * Prefix
     ValidPrefix
   , ToPrefix(..)
-  -- * Helpers
-  , LengthLT64C
-  , IsLUSymbolC
-  , LengthSymbol
-  , IsLowerChar
-  , IsUnderscore
-  , IsLUSymbol
-  , ILUSUH1
-  , ILUSUH2
-  -- * Deprecated Helpers
-  , IsLowerSymbol
-  , LSUH
-  , ILSUH
   ) where
 
+#ifndef __HADDOCK_VERSION__
 import           Data.Kind
+import           Data.KindID.Error
 import           Data.Type.Bool
 import           Data.Type.Equality
 import           Data.Type.Ord
+import           Data.TypeID.Error
+#endif
 import           GHC.TypeLits
 
 -- | A class that translates any kind to a 'Symbol'. It is used to translate
@@ -72,77 +64,29 @@ class ToPrefix a where
 instance ToPrefix (s :: Symbol) where
   type PrefixSymbol s = s
 
--- | A constraint for valid prefix 'Symbol's.
+#ifndef __HADDOCK_VERSION__
 type ValidPrefix prefix = ( KnownSymbol prefix
                           , LengthLT64C prefix
                           , IsLUSymbolC prefix )
+#else
+-- | A constraint for valid prefix 'Symbol's.
+--
+-- Note that this is __NOT__ the actual definition! It's true definition is
+-- hidden here in the documentation as it uses internal type-level helpers that
+-- we do not expose and make no guarantee on their In practice, any prefix with
+-- this constraint is a valid prefix for a 'Data.KindID.KindID'.
+type ValidPrefix prefix = KnownSymbol prefix
+#endif
 
+#ifndef __HADDOCK_VERSION__
 -- | Contains a custom error message if the prefix 'Symbol' is too long.
 type family LengthLT64C (prefix :: Symbol) :: Constraint where
   LengthLT64C s
     = If (Compare (LengthSymbol s) 64 == 'LT) (() :: Constraint)
-         ( TypeError ( Text "The prefix "
-                  :<>: ShowType s
-                  :<>: Text " with "
-                  :<>: ShowType (LengthSymbol s)
-                  :<>: Text " characters is too long!" ) )
+         (TypeError (ToErrorMessage ('TypeIDErrorPrefixTooLong (Sym2Str s))))
 
 -- | Contains a custom error message if the prefix 'Symbol' is not lowercase +
 -- underscore or it starts or ends with underscores.
 type family IsLUSymbolC (prefix :: Symbol) :: Constraint where
-  IsLUSymbolC s
-    = If (IsLUSymbol s) (() :: Constraint)
-         ( TypeError ( Text "The prefix "
-                  :<>: ShowType s
-                  :<>: Text " is not valid!" ) )
-
--- | The length of a 'Symbol' as a 'Nat'.
-type family LengthSymbol (prefix :: Symbol) :: Nat where
-  LengthSymbol prefix = LSUH (UnconsSymbol prefix)
-
--- | LengthSymbol Uncons Helper.
-type family LSUH (uncons :: Maybe (Char, Symbol)) :: Nat where
-  LSUH 'Nothing          = 0
-  LSUH ('Just '( c, s )) = 1 + LengthSymbol s
-
--- | Is a type-level 'Char' lowercase?
-type family IsLowerChar (ch :: Char) :: Bool where
-  IsLowerChar ch = Compare '`' ch == 'LT && Compare ch '{' == 'LT
-
--- | Is a type-level 'Char' an underscore?
-type family IsUnderscore (ch :: Char) :: Bool where
-  IsUnderscore ch = Compare '_' ch == 'EQ
-
--- | Is a 'Symbol' lowercase + underscore and not start or end with underscores?
-type family IsLUSymbol (prefix :: Symbol) :: Bool where
-  IsLUSymbol prefix = ILUSUH1 (UnconsSymbol prefix)
-
--- | First IsLUSymbol Uncons Helper.
-type family ILUSUH1 (uncons :: Maybe (Char, Symbol)) :: Bool where
-  ILUSUH1 'Nothing            = True
-  ILUSUH1 ('Just '( '_', _ )) = False
-  ILUSUH1 ('Just '( c, s ))   = (IsLowerChar c || IsUnderscore c)
-                             && ILUSUH2 (UnconsSymbol s)
-
--- | Second IsLUSymbol Uncons Helper.
-type family ILUSUH2 (uncons :: Maybe (Char, Symbol)) :: Bool where
-  ILUSUH2 'Nothing           = True
-  ILUSUH2 ('Just '( c, "" )) = IsLowerChar c
-  ILUSUH2 ('Just '( c, s ))  = (IsLowerChar c || IsUnderscore c)
-                            && ILUSUH2 (UnconsSymbol s)
-
-
---------------------------------------------------------------------------------
--- Deprecated
---------------------------------------------------------------------------------
-
--- | Is a 'Symbol' lowercase?
-type family IsLowerSymbol (prefix :: Symbol) :: Bool where
-  IsLowerSymbol prefix = ILSUH (UnconsSymbol prefix)
-{-# DEPRECATED IsLowerSymbol "No longer used; to be removed in version 0.7" #-}
-
--- | Is LowerSymbol Uncons Helper.
-type family ILSUH (uncons :: Maybe (Char, Symbol)) :: Bool where
-  ILSUH 'Nothing          = 'True
-  ILSUH ('Just '( c, s )) = IsLowerChar c && IsLowerSymbol s
-{-# DEPRECATED ILSUH "No longer used; to be removed in the version 0.7" #-}
+  IsLUSymbolC s = BuildTypeIDErrorConstraint (IsLUSymbol s)
+#endif
---
+++ src/Data/KindID/Error.hs
@@ -0,0 +1,100 @@
+{-# LANGUAGE UndecidableInstances #-}
+
+-- | An internal module to provide better compilation errors for invalid
+-- 'Data.KindID.V7.KindID' prefixes.
+--
+module Data.KindID.Error where
+
+import           Data.Kind
+import           Data.TypeID.Error
+import           Data.Type.Bool
+import           Data.Type.Equality
+import           Data.Type.Ord
+import           GHC.TypeLits
+
+class ToErrorMessageC (e :: k) where
+  type ToErrorMessage e :: ErrorMessage
+
+instance ToErrorMessageC (TypeIDErrorPrefixTooLong t) where
+  type ToErrorMessage (TypeIDErrorPrefixTooLong t) = 
+        Text "The prefix "
+   :<>: ShowType (Str2Sym t)
+   :<>: Text " with "
+   :<>: ShowType (LengthSymbol (Str2Sym t))
+   :<>: Text " characters is too long!"
+
+instance ToErrorMessageC TypeIDExtraSeparator where
+  type ToErrorMessage TypeIDExtraSeparator = Text
+    "The underscore separator should not be present if the prefix is empty!"
+
+type family Str2Sym (str :: String) :: Symbol where
+  Str2Sym '[]       = ""
+  Str2Sym (c ': cs) = ConsSymbol c (Str2Sym cs)
+
+type family Sym2Str (str :: Symbol) :: String where
+  Sym2Str s = SSUH (UnconsSymbol s)
+
+type family SSUH (uncons :: Maybe (Char, Symbol)) :: String where
+  SSUH Nothing          = '[]
+  SSUH (Just '( c, s )) = c ': Sym2Str s
+
+-- | The length of a 'Symbol' as a 'Nat'.
+type family LengthSymbol (prefix :: Symbol) :: Nat where
+  LengthSymbol prefix = LSUH (UnconsSymbol prefix)
+
+-- | LengthSymbol Uncons Helper.
+type family LSUH (uncons :: Maybe (Char, Symbol)) :: Nat where
+  LSUH 'Nothing          = 0
+  LSUH ('Just '( c, s )) = 1 + LengthSymbol s
+
+-- | Is a type-level 'Char' lowercase?
+type family IsLowerChar (ch :: Char) :: Bool where
+  IsLowerChar ch = Compare '`' ch == 'LT && Compare ch '{' == 'LT
+
+-- | Is a type-level 'Char' an underscore?
+type family IsUnderscore (ch :: Char) :: Bool where
+  IsUnderscore ch = Compare '_' ch == 'EQ
+
+-- | Is a 'Symbol' lowercase + underscore and not start or end with underscores?
+--
+-- ''Nothing' if no error; otherwise ''Just' a 'TypeIDError'.
+type family IsLUSymbol (prefix :: Symbol) :: Maybe TypeIDError where
+  IsLUSymbol "_"    = 'Just 'TypeIDExtraSeparator
+  IsLUSymbol prefix = ILUSUH1 (UnconsSymbol prefix) prefix
+
+-- | First IsLUSymbol Uncons Helper.
+type family ILUSUH1 (uncons :: Maybe (Char, Symbol)) (prefix :: Symbol)
+  :: Maybe TypeIDError where
+    ILUSUH1 'Nothing s            = 'Nothing
+    ILUSUH1 ('Just '( '_', _ )) s = 'Just
+      (TypeIDStartWithUnderscore (Sym2Str s))
+    ILUSUH1 ('Just '( c, r )) s   =
+          WrapMaybe (IsLowerChar c || IsUnderscore c)
+                    (TypeIDErrorPrefixInvalidChar (Sym2Str s) c)
+      <|> ILUSUH2 (UnconsSymbol r) s
+
+-- | Second IsLUSymbol Uncons Helper.
+type family ILUSUH2 (uncons :: Maybe (Char, Symbol)) (prefix :: Symbol)
+  :: Maybe TypeIDError where
+    ILUSUH2 'Nothing s             = 'Nothing
+    ILUSUH2 ('Just '( '_', "" )) s = 'Just (TypeIDEndWithUnderscore (Sym2Str s))
+    ILUSUH2 ('Just '( c, r )) s    =
+          WrapMaybe (IsLowerChar c || IsUnderscore c)
+                    (TypeIDErrorPrefixInvalidChar (Sym2Str s) c)
+      <|> ILUSUH2 (UnconsSymbol r) s
+
+-- | @wrapMaybe True _ = Nothing; wrapMaybe False x = Just x@.
+type family WrapMaybe p a where
+  WrapMaybe True a  = 'Nothing
+  WrapMaybe False a = 'Just a
+
+-- | Type-level '(<|>)' for 'Maybe'.
+type family (<|>) a b where
+  'Just a <|> b = 'Just a
+  Nothing <|> b = b
+
+-- | Turn a type-level 'Maybe' 'TypeIDError' into a 'TypeError'.
+type family BuildTypeIDErrorConstraint (a :: Maybe TypeIDError)
+  :: Constraint where
+    BuildTypeIDErrorConstraint 'Nothing  = ()
+    BuildTypeIDErrorConstraint ('Just e) = TypeError (ToErrorMessage e)
